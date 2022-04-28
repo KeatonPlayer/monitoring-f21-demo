ae098f5fc2114bba4c89885d149e0275daed7423
Keaton Player <keatonplayer18@gmail.com>
2022-04-28 12:08:27 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+const path = require('path')
+const app = express()
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+app.use(express.static('public'))
+const port  = process.env.PORT || 4005
+app.listen(port, function(){
+console.log(`Listening on da port that is ${port}`)
+})
