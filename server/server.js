7b723d6b24263b144d44e03b1f5dec6fc89176dd
Keaton Player <keatonplayer18@gmail.com>
2022-04-28 12:33:00 -0600

    test

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const express = require ('express')
 const app = express()
+let Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+  accessToken: '74cec7add9b745f8b89797ead7674de3',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+rollbar.log('Hello world!')
+
 app.use(express.static('public'))
 const port  = process.env.PORT || 4005
 app.listen(port, function(){
