c4120baba537cf76870d25242d18b33d7914e848
Keaton Player <keatonplayer18@gmail.com>
2022-04-28 14:31:22 -0600

    added stuff

--- server/server.js
+++ server/server.js
@@ -8,7 +8,52 @@ var rollbar = new Rollbar({
 })
 rollbar.log('Hello world!')
 app.use(express.static('public'))
+
+app.use(express.json())
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/api/students', (req, res) => {
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+           res.status(400).send('You must enter a name.')
+       } else {
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    res.status(200).send(students)
+})
+
+
+
+
+
+
+
+
 const port  = process.env.PORT || 4005
 app.listen(port, function(){
 console.log(`Listening on da port that is ${port}`)
 })
+
